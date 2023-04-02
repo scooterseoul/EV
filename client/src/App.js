28b900ab311d1556241fc37fad56e97079f96386
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2023-04-02 17:26:34 +0900

    hero pic

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import AllCars from "./components/AllCars";
 import SingleCar from "./components/SingleCar";
 import AddCar from "./components/AddCar";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<AllCars />} />
         <Route path="/cars/:id" element={<SingleCar />} />
