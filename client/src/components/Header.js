1cc0048368001802a19345586863eb6fa86490f1
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2023-04-02 14:30:32 +0900

    Header css

---
+++ client/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Header.module.css";
+import headerimg from "../images/headerBanner.png";
+import mail from "../images/icons8-secured-letter-64.png";
+import logo from "../images/logo2.png";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.navbar}>
+        <img src={logo} className={styles.logo} />
+        <div className={styles.mailCont}>
+          <img
+            src={mail}
+            className={styles.mail}
+            onClick={() => (window.location = "mailto: scooterseoul@gmail.com")}
+          />
+        </div>
+      </div>
+      <img src={headerimg} className={styles.headerimg} />
+    </div>
+  );
+};
+
+export default Header;
